25f3f772a4e55b46aa286acd761270782d92293d
buyili <1132989278@qq.com>
2025-05-05 18:23:17 +0800

    Update output message

--- scripts/copy_release.ps1
+++ scripts/copy_release.ps1
@@ -1,21 +1,21 @@
 $sourcePath = "$PSScriptRoot\..\build\windows\x64\runner\Release"
 $destinationPath = "D:/src/WallpaperTools"
 
-Write-Host "开始构建 Windows 项目..."
+Write-Host "=== Starting to build the Windows project... ==="
 flutter build windows
 
 # 检查目标目录是否存在，如果不存在则创建
-Write-Host "正在检查目标目录是否存在..."
+Write-Host "=== Checking if the destination directory exists... ==="
 if (-not (Test-Path -Path $destinationPath)) {
-    Write-Host "目标目录不存在，正在创建..."
+    Write-Host "=== The destination directory does not exist. Creating it now... ==="
     New-Item -ItemType Directory -Path $destinationPath
 }
 
 # 删除目标目录内容
-Write-Host "正在删除目标目录内容..."
+Write-Host "=== Deleting the contents of the destination directory... ==="
 Remove-Item -Path $destinationPath\* -Recurse -Force
 
 # 复制文件和文件夹
-Write-Host "正在复制文件和文件夹..."
+Write-Host "=== Copying files and folders... ==="
 Copy-Item -Path $sourcePath\* -Destination $destinationPath -Recurse -Force
-Write-Host "文件复制完成。"
+Write-Host "=== File copying completed. ==="
